9c77d3bb77e9e53d5f4ad04e6aabaa1d1513f81e
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2009-06-15 18:05:25 -0300

    fixed bug in finders definition

--- lib/enum_field/define_enum.rb
+++ lib/enum_field/define_enum.rb
@@ -3,14 +3,14 @@ module EnumField
     def define_enum(&block)
       @enum_builder ||= EnumField::Builder.new(self)
       yield @enum_builder
-    end
 
-    [:all, :find_by_id, :find, :first, :last].each do |method|
-      module_eval <<-END
-        def #{method}(*args, &block)
-          @enum_builder.send(:#{method}, *args, &block)
-        end
-      END
+      [:all, :find_by_id, :find, :first, :last].each do |method|
+        instance_eval <<-END
+          def #{method}(*args, &block)
+            @enum_builder.send(:#{method}, *args, &block)
+          end
+        END
+      end
     end
   end
 end
